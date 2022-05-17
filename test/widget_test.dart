a1c404ab3e5ab057698440bcf55568ebc49712d0
Akshay Siddannavar <72383774+AkshaySiddannavar@users.noreply.github.com>
2022-05-17 12:05:10 +0530

    add files for each screen
    Added 1 file for each screen in app.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:give_easy/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const GiveEasy());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
