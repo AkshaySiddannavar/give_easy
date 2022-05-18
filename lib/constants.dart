b9c4e6dd5388134823713e12da4103af7ffd4a89
Akshay Siddannavar <72383774+AkshaySiddannavar@users.noreply.github.com>
2022-05-18 10:41:28 +0530

    improve kInputTextFieldDecoration
    Added required changes to KInputTextFieldDecoration for making it look
    good for MVP.

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,1 +1,18 @@
 import 'package:flutter/material.dart';
+
+const kInputTextFieldDecoration = InputDecoration(
+  hintText: 'Hint Text Here',
+  contentPadding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 8.0),
+  border: OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.black87),
+    borderRadius: BorderRadius.all(
+      Radius.circular(20.0),
+    ),
+  ),
+  focusedBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.black87),
+    borderRadius: BorderRadius.all(
+      Radius.circular(20.0),
+    ),
+  ),
+);
