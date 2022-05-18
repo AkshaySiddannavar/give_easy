c250c7069953bfc377e5ac9210bac4e47852ee80
Akshay Siddannavar <72383774+AkshaySiddannavar@users.noreply.github.com>
2022-05-18 16:05:35 +0530

    add login button
    Added login button in login.dart, though functionality is yet to be
    added.

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:give_easy/components/action_button.dart';
 import 'package:give_easy/constants.dart';
 import 'package:give_easy/components/input_text_field.dart';
 
@@ -13,20 +14,20 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen>
     with SingleTickerProviderStateMixin {
-  late AnimationController heroIconController;
+  // late AnimationController heroIconController;
   @override
   void initState() {
     super.initState();
-    heroIconController = AnimationController(
-        vsync: this,
-        duration: Duration(seconds: 2),
-        lowerBound: 50.0,
-        upperBound: 150.0);
+    // heroIconController = AnimationController(
+    //     vsync: this,
+    //     duration: Duration(seconds: 2),
+    //     lowerBound: 50.0,
+    //     upperBound: 150.0);
 
-    heroIconController.forward();
-    heroIconController.addListener(() {
-      setState(() {});
-    });
+    // heroIconController.forward();
+    // heroIconController.addListener(() {
+    //   setState(() {});
+    // });
   }
 
   @override
@@ -34,33 +35,49 @@ class _LoginScreenState extends State<LoginScreen>
     return Scaffold(
       body: SafeArea(
         child: Center(
-          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-            Hero(
-              tag: 'appIconTransition',
-              child: SizedBox(
-                height: heroIconController.value,
-                child: FaIcon(
-                  size: heroIconController.value,
-                  FontAwesomeIcons.handshake,
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Flexible(
+                  child: Hero(
+                    tag: 'appIconTransition',
+                    child: SizedBox(
+                      height: 200.0,
+                      child: Image.asset('assets/images/handshake.png'),
+                    ),
+                  ),
                 ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: InputTextField(
-                hintWord: 'Enter Email',
-                isObscure: false,
-              ),
-            ),
-            SizedBox(
-              height: 5.0,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child:
-                  InputTextField(hintWord: 'Enter Password', isObscure: true),
-            ),
-          ]),
+                const SizedBox(
+                  height: 30.0,
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: InputTextField(
+                      hintWord: 'Enter Email',
+                      isObscure: false,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 5.0,
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: InputTextField(
+                        hintWord: 'Enter Password', isObscure: true),
+                  ),
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                ActionButton(
+                  buttonText: 'Login',
+                  buttonActionCallback: () {},
+                ),
+              ]),
         ),
       ),
     );
