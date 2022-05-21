10be8922d169d63389aefacbaa42697785c4c9c6
Akshay Siddannavar <72383774+AkshaySiddannavar@users.noreply.github.com>
2022-05-21 23:20:03 +0530

    add preview card
    Because:
    - It will contain preview image of each specific donation/donation request created
    - Increase reusability
    - Increase modularity
    - It will be part of CategoricalTile which will contain many such preview cards aligned horizontally
    
    Added preview_card.dart file. It contains a stateless widget which will contain preview images for their specific donations/donation requests. For now this contains arbitrary colors for identifying size and shape of containers.

---
+++ lib/components/preview_card.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:give_easy/constants.dart';
+
+class PreviewCard extends StatelessWidget {
+  final Image previewImage; //image shown in PreviewCard
+
+  const PreviewCard({
+    Key? key,
+    this.previewImage = kDefaultPreviewImage,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      //outer container
+      height: 125.0,
+      width: 125.0,
+      //height and width such that at least 3 of PreviewCards should be visible by default
+      //change it later in case required
+
+      padding: EdgeInsets.all(10.0),
+      child: Container(
+        //inner container
+        //replace it with padding later on if required
+
+        decoration: BoxDecoration(
+            color: Colors.redAccent, //to see extent of container
+
+            borderRadius: BorderRadius.all(Radius.circular(20.0))),
+
+        child: Flex(
+            direction:
+                Axis.horizontal, //change direction in case required later on
+
+            children: [Flexible(child: previewImage)]
+            //We use Flex > Flexible so that we can change size of image from its original size
+            ),
+      ),
+      decoration: BoxDecoration(
+          color: Colors.tealAccent, //to identify extent of outer container
+          borderRadius: BorderRadius.all(Radius.elliptical(20.0, 20.0))),
+    );
+  }
+}
