d46e0f4a5b296989b1554179d8b87cda97867ae2
Akshay Siddannavar <72383774+AkshaySiddannavar@users.noreply.github.com>
2022-05-22 14:25:59 +0530

    use categorical tile and preview card for checking
    Because:
    - To check layout of categorical tile and preview card
    - To check behaviour of categorical tile and preview card when used in home_screen
    - Easier debugging
    
    Added categorical tile and preview card to home screen.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:give_easy/components/categorical_tile.dart';
+import 'package:give_easy/components/preview_card.dart';
 
 class HomeScreen extends StatefulWidget {
   static const id = "home_screen";
@@ -13,13 +15,18 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        //make it transparent or translucent
+        shadowColor: Colors.transparent,
+        scrolledUnderElevation: 1.0,
         elevation: 0.0,
         bottomOpacity: 1.0,
-        toolbarOpacity: 0.0,
+        toolbarOpacity: 1.0,
         backgroundColor: Colors.transparent,
+        foregroundColor: Colors.transparent,
         title: Text(
           'Title',
-          style: TextStyle(color: Colors.black),
+          style: TextStyle(
+              color: Colors.black, backgroundColor: Colors.transparent),
         ),
         leading: //Profile Icon/Pic will come here
             Container(
@@ -44,6 +51,75 @@ class _HomeScreenState extends State<HomeScreen> {
               tag: 'appIconTransition',
               child: Image.asset('assets/images/handshake.png'),
             ),
+          ),
+        ],
+      ),
+      body: ListView(
+        children: [
+          Column(
+            children: [
+              PreviewCard(
+                previewImage: Image.asset('assets/images/handshake.png'),
+              ),
+
+              CategoricalTile(
+                categoryName: 'Category 1',
+                categorySpecificList: [
+                  PreviewCard(
+                    previewImage: Image.asset('assets/images/handshake.png'),
+                  ),
+                  PreviewCard(
+                    previewImage: Image.asset('assets/images/handshake.png'),
+                  ),
+                  PreviewCard(
+                    previewImage: Image.asset('assets/images/handshake.png'),
+                  ),
+                  PreviewCard(
+                    previewImage: Image.asset('assets/images/handshake.png'),
+                  ),
+                  PreviewCard(
+                    previewImage: Image.asset('assets/images/handshake.png'),
+                  ),
+                  PreviewCard(
+                    previewImage: Image.asset('assets/images/handshake.png'),
+                  ),
+                ],
+              ),
+              Text(
+                '1',
+                style: TextStyle(fontSize: 100.0),
+              ),
+              Text(
+                '1',
+                style: TextStyle(fontSize: 100.0),
+              ),
+              Text(
+                '1',
+                style: TextStyle(fontSize: 100.0),
+              ),
+              Text(
+                '1',
+                style: TextStyle(fontSize: 100.0),
+              ),
+              Text(
+                '1',
+                style: TextStyle(fontSize: 100.0),
+              ),
+              Text(
+                '1',
+                style: TextStyle(fontSize: 100.0),
+              ),
+              // ListView.builder(
+              //   itemCount: list.length,
+              //   //this list contains [[category, horizontal-list],[category, horizontal-list],[category, horizontal-list],...]
+
+              //   itemBuilder: (BuildContext context, int index) {
+              //     return CustomWidget(
+              //         categoryTitle: '#categroy1',
+              //         listToBeRendered: list[index].horizontal - list);
+              //   },
+              // ),
+            ],
           ),
         ],
       ),
