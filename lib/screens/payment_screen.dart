bbaa6b31a89e46f098fe7fc66141ced5f32154a8
Akshay Siddannavar <72383774+AkshaySiddannavar@users.noreply.github.com>
2022-05-27 21:04:04 +0530

    add currency parameter and add print statements to display information of actions
    Added INR as currency in options. Then added print statements for each
    event which will disply appropriate data on screen on that event.

--- lib/screens/payment_screen.dart
+++ lib/screens/payment_screen.dart
@@ -28,13 +28,16 @@ class _PaymentScreenState extends State<PaymentScreen> {
       //Hence we need to multiply by 100 to get rupee equivalent
       'name': 'Unknown NGO', //name of receiver
       'description': 'Donation to some cause',
+      'currency': 'INR',
+      'wallet': 'paytm',
       'prefill': {
         'contact': '8888888888',
         'email': 'sample@sample.com',
       }
-      //TODO: 1. checkout razorpay payment APIs for knowing what all parameters are there in options
-      //TODO: 2. see what all parameters are useful to you
-      //TODO: 3. use those paremeters
+      //done - all parameters : https://razorpay.com/docs/payments/payment-gateway/flutter-integration/standard/build-integration/#17-add-checkout-options
+
+      //done - see what all parameters are useful to you
+      //done - use those paremeters
     };
 
     _razorpay.open(options);
@@ -48,24 +51,27 @@ class _PaymentScreenState extends State<PaymentScreen> {
     _razorpay.on(Razorpay.EVENT_EXTERNAL_WALLET, _handleExternalWallet);
   }
 
-  //TODO: 3.5 understand PaymentSuccessResponse object so that you can use its parameters properly
-  //TODO: 3.6 understand PaymentFailureResponse object so that you can use its parameters properly
-  //TODO: 3.7 understand ExternalWalletResponse object so that you can use its parameters properly
+  //understand PaymentSuccessResponse object so that you can use its parameters properly
+  //understand PaymentFailureResponse object so that you can use its parameters properly
+  //understand ExternalWalletResponse object so that you can use its parameters properly
 
   void _handlePaymentSuccess(PaymentSuccessResponse response) {
     //TODO: 4. Do something when payment succeeds, show a widget of confirmation and re-direct to Thank You screen
-    print('');
+    print(
+        'Order ID : ${response.orderId} \nPayment ID : ${response.paymentId} \nSignature: ${response.signature}');
+    //order ID : null | Payment ID : somevlaue | Signature: null
   }
 
   void _handlePaymentError(PaymentFailureResponse response) {
     //TODO: 5. Do something when payment fail, show a widget of confirmation and re-direct to some error information screen(to be createad)
 
-    print('');
+    print(
+        'Error Code : ${response.code} \nError Message : ${response.message}');
   }
 
   void _handleExternalWallet(ExternalWalletResponse response) {
     //TODO: 6. Do something when an external wallet was selected, at least print data of wallet
-    print('');
+    print('External Wallet : ${response.walletName}');
   }
 
   //TODO: 7. Add exception handling
