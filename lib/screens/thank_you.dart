253a00f5d88fc5a52a12a469950f7993543c0a4f
Akshay Siddannavar <72383774+AkshaySiddannavar@users.noreply.github.com>
2022-05-28 13:08:48 +0530

    add content of thank you screen
    Because:
    - User should see a message on thank you screen after successful
      completion of transaction
    - User will be assured that money has been donated
    
    Added content of thank you screen.

--- lib/screens/thank_you.dart
+++ lib/screens/thank_you.dart
@@ -1,1 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:give_easy/components/action_button.dart';
+import 'package:give_easy/screens/all_screens.dart';
+
+class ThankYouScreen extends StatelessWidget {
+  static const String id = 'thank_you_screen';
+  final String ngoName;
+  const ThankYouScreen({Key? key, this.ngoName = ''}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Material(
+        child: Flex(
+          direction: Axis.vertical,
+          children: [
+            Flexible(
+                fit: FlexFit.loose,
+                child: Text(
+                  'Thanks A Lot\nFor Your Loving Donation! 😄',
+                  style: TextStyle(fontSize: 40.0),
+                  textAlign: TextAlign.center,
+                )),
+            SizedBox(
+              height: 30.0,
+            ),
+            Flexible(
+              fit: FlexFit.loose,
+              child: Text(
+                'We at #${ngoName} are really grateful to you for this generous donation.',
+                style: TextStyle(fontSize: 20.0),
+              ),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Flexible(
+              fit: FlexFit.loose,
+              child: Text(
+                'Your donation will be used to futher the cause that matters to you ❤️',
+                style: TextStyle(
+                  fontSize: 15.0,
+                  color: Colors.grey,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            ActionButton(
+                buttonText: 'Go Back To Home',
+                buttonActionCallback: () {
+                  Navigator.pushNamed(context, HomeScreen.id);
+                }),
+          ],
+        ),
+      ),
+    );
+  }
+}
