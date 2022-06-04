ae4bd83269524d75854168076b05862b4bf267ee
Akshay Siddannavar <72383774+AkshaySiddannavar@users.noreply.github.com>
2022-06-04 11:45:04 +0530

    add routes to some screens directly or indirectly accessible through drawer
    Because:
    - For easier debugging
    - For navigation
    
    Added routes to :
    1. create_requests_screen
    2. current_request_screen
    3. past_requests_screen
    4. profile_screen
    5. validation_screen
    6. your_gives_screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:give_easy/screens/all_screens.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:give_easy/screens/create_request.dart';
+import 'package:give_easy/screens/current_request.dart';
+import 'package:give_easy/screens/past_requests.dart';
 import 'package:give_easy/screens/payment_screen.dart';
+import 'package:give_easy/screens/profile.dart';
+import 'package:give_easy/screens/validation.dart';
+import 'package:give_easy/screens/your_gives.dart';
 import '../firebase_options.dart';
 
 void main() async {
@@ -25,7 +31,13 @@ class GiveEasy extends StatelessWidget {
         SpecificDonationScreen.id: (context) => SpecificDonationScreen(),
         PaymentScreen.id: (context) => PaymentScreen(),
         ThankYouScreen.id: (context) => ThankYouScreen(),
-        //add route to PaymentScreen
+        CreateRequestScreen.id: (context) => CreateRequestScreen(),
+        CurrentRequestScreen.id: (context) => CurrentRequestScreen(),
+        PastRequestsScreen.id: (context) => PastRequestsScreen(),
+        ProfileScreen.id: (context) => ProfileScreen(),
+        ValidationScreen.id: (context) => ValidationScreen(),
+        YourGivesScreen.id: (context) => YourGivesScreen(),
+        //add route to
         //becasue : - for easier debugging, for navigation
       },
     );
