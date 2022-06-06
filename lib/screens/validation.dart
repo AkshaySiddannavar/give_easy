82ca4c0c8638b0dacf37c9d11059aa90f67441b9
Akshay Siddannavar <72383774+AkshaySiddannavar@users.noreply.github.com>
2022-06-06 15:27:15 +0530

    create a firebase-storage instance and a reference to firebase-storage
    Because:
    - We need a way to interact with firebase-storage and a firebase-storage instance is for
      that
    - We need a way to point to specific locations into the storage
    - We need a way to manipulate data and perform operations on data in
      firebase-storage we have a reference for this
    
    Created a firebase-storage instance and created a reference to firebase-storage.
    The reference will help us in creating a pointer to specific locations in storage and help us in manipulating data in firebase-storage.

--- lib/screens/validation.dart
+++ lib/screens/validation.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 class ValidationScreen extends StatelessWidget {
   static const String id = 'validation_screen';
 
-  const ValidationScreen({Key? key}) : super(key: key);
+  ValidationScreen({Key? key}) : super(key: key);
+
+  final storage = FirebaseStorage.instance;
+  final storageRef = FirebaseStorage.instance.ref();
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        color: Colors.orange,
+        //something which can be used to test if files are uploaded on google storage (do it later along with docs)
+      ),
+    );
   }
 }
