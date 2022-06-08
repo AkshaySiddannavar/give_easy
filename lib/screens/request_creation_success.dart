148c819ec0681ef81ad83b88fb23bf7ff58efe6c
Akshay Siddannavar <72383774+AkshaySiddannavar@users.noreply.github.com>
2022-06-08 13:39:19 +0530

    add id property for screen
    Because:
    - Easier navigation
    - Easier debugging
    
    Added id property to request_creation_success screen.

---
+++ lib/screens/request_creation_success.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class RequestCreationSuccessScreen extends StatelessWidget {
+  static const String id = 'request_creation_success_screen';
+  const RequestCreationSuccessScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
