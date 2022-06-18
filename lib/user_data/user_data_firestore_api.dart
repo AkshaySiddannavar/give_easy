bc55cd3cce4496b87ca7e57282b75ca3def53ff6
Akshay Siddannavar <72383774+AkshaySiddannavar@users.noreply.github.com>
2022-06-18 13:30:35 +0530

    add functionality to read user's data
    Because:
    - We wanted to display user's data in profile
    
    Added functionality to read user's data.

--- lib/user_data/user_data_firestore_api.dart
+++ lib/user_data/user_data_firestore_api.dart
@@ -3,6 +3,7 @@
 //* implement all firebase firestore operations/triggering cloud functions for user-data collection here
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class UserDataFirestoreAPI {
   String gender;
@@ -10,13 +11,14 @@ class UserDataFirestoreAPI {
   String phoneNumber;
   String userEmail;
   String userName;
-
+  String uniqueID;
   UserDataFirestoreAPI(
       {required this.userName,
       required this.phoneNumber,
       this.organizationName = '', //empty string means no organization
       required this.gender,
-      required this.userEmail});
+      required this.userEmail,
+      required this.uniqueID});
 
   //Collection Name : user-data
 
@@ -29,21 +31,38 @@ class UserDataFirestoreAPI {
       "phoneNumber": phoneNumber,
       "userEmail": userEmail,
       "userName": userName,
+      "uniqueID": uniqueID,
     };
 
     var _firestore = FirebaseFirestore.instance;
     _firestore.collection('user-data').doc().set(userData);
-
-    print("**************\n ${userData}\n******* UPLOADED");
   }
 
-  //TODO: Read user's data from firestore user-data collection
-  void readUserData() {
-    var _firestore = FirebaseFirestore.instance;
-    // _firestore.collection('user-data').doc(); I need to find a document where either email = email or I could use userID for unique identification
+  //Read user's data from firestore user-data collection
+  static Future<Map<String, dynamic>> readUserData() async {
+    var uid = FirebaseAuth.instance.currentUser!.uid;
 
-    // print("**************\n ${}\n******* UPLOADED");
+    var _firestore = FirebaseFirestore.instance;
+
+    final userDataRef =
+        _firestore.collection('user-data'); //path to user-data collection
+
+    final readQuery =
+        _firestore.collection('user-data').where("uniqueID", isEqualTo: uid);
+    //I need to find a document where either email = email or I could use userID for unique identification
+    //currently I am using UID for identification though in case required it can be changed to email
+
+    final resultQuerySnapshot = await readQuery.get().then(
+          (resultOfQuery) => resultOfQuery,
+          onError: (error) => print('error $error'),
+        ); //executing query
+    //resultQuerySnapshot.docs[0].exists = true if document for given user exists which it will until and unless there has been some error during registeration
+
+    var userDataMap =
+        resultQuerySnapshot.docs[0].data(); //all the user's data is stored here
+
+    return userDataMap;
   }
 }
 
-//TODO add Account deletion feature (later)
+//add Account deletion feature (later)
