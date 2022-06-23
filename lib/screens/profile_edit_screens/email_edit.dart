5ce5052c5c17efd8ed3d87b8ca64d565d37ad5e1
Akshay Siddannavar <72383774+AkshaySiddannavar@users.noreply.github.com>
2022-06-23 18:38:50 +0530

    add email_edit screen functionality
    Because:
    -We need a screen where user can edit and submit new email to firestore
    
    Added functionality for email_edit screen.

--- lib/screens/profile_edit_screens/email_edit.dart
+++ lib/screens/profile_edit_screens/email_edit.dart
@@ -1,11 +1,84 @@
 import 'package:flutter/material.dart';
+import 'package:give_easy/components/action_button.dart';
+import 'package:give_easy/components/input_text_field.dart';
+import 'package:give_easy/user_data/user_data_change_notifier.dart';
+import 'package:provider/provider.dart';
 
-class EmailEditScreen extends StatelessWidget {
+class EmailEditScreen extends StatefulWidget {
   static const String id = 'email_edit_screen';
   const EmailEditScreen({Key? key}) : super(key: key);
 
   @override
+  State<EmailEditScreen> createState() => _EmailEditScreenState();
+}
+
+class _EmailEditScreenState extends State<EmailEditScreen> {
+  String emailTyped = '';
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Consumer<userData>(
+        builder: (context, userDataObject, child) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Email Edit'),
+            ),
+            body: Container(
+              padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 10.0),
+              child: Form(
+                child: Column(
+                  children: [
+                    Expanded(
+                      child: Padding(
+                        padding: EdgeInsets.all(10),
+                        child: InputTextField(
+                          isReadOnly: true,
+                          hintMessage: '',
+                          isSensitive: false,
+                          currentTextCallback: (currentText) {},
+                          textFieldLabel: 'Current Email',
+                          defaultText: userDataObject.profileData["userEmail"],
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: EdgeInsets.all(10),
+                        child: InputTextField(
+                          hintMessage: 'Enter New Email',
+                          isSensitive: false,
+                          currentTextCallback: (currentText) {
+                            emailTyped = currentText;
+                            print(currentText);
+                            setState(() {});
+                          },
+                          textFieldLabel: 'New Email',
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    ActionButton(
+                        isActive: (emailTyped != null && emailTyped.isNotEmpty)
+                            ? true
+                            : false,
+                        buttonText: 'Update Email',
+                        buttonActionCallback: () {
+                          userDataObject.updateUserEmail(emailTyped);
+                          FocusScope.of(context)
+                              .unfocus(); //To disable keyboard before screen change so that we don't get a overflow exception
+
+                          Navigator.pop(context);
+                        })
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
   }
 }
