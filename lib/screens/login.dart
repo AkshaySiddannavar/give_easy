57b1fd6b135409d8e9d3d7a79c1afe77ae34f319
Akshay Siddannavar <72383774+AkshaySiddannavar@users.noreply.github.com>
2022-06-23 20:34:40 +0530

    add Consumer to login screen
    Because:
    - We want to update the profile data which was initially exposed by ChangeNotifierProvider
    - We want to supply actual values of user's data when he logs into the app
    
    Added Consumer to login screen.

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -5,6 +5,8 @@ import 'package:give_easy/constants.dart';
 import 'package:give_easy/components/input_text_field.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:give_easy/screens/all_screens.dart';
+import 'package:give_easy/user_data/user_data_change_notifier.dart';
+import 'package:provider/provider.dart';
 
 class LoginScreen extends StatefulWidget {
   static const String id = 'login_screen';
@@ -19,71 +21,78 @@ class _LoginScreenState extends State<LoginScreen> {
   late String email, password;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Flexible(
-                  child: Hero(
-                    tag: 'appIconTransition',
-                    child: SizedBox(
-                      height: 200.0,
-                      child: Image.asset('assets/images/handshake.png'),
+    return Consumer<userData>(
+      builder: (context, userDataObject, child) => Scaffold(
+        body: SafeArea(
+          child: Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Flexible(
+                    child: Hero(
+                      tag: 'appIconTransition',
+                      child: SizedBox(
+                        height: 200.0,
+                        child: Image.asset('assets/images/handshake.png'),
+                      ),
                     ),
                   ),
-                ),
-                const SizedBox(
-                  height: 30.0,
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: InputTextField(
-                      type: TextInputType.emailAddress,
-                      hintMessage: 'Enter Email',
-                      isSensitive: false,
-                      currentTextCallback: (value) {
-                        email = value;
-                      },
+                  const SizedBox(
+                    height: 30.0,
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: InputTextField(
+                        type: TextInputType.emailAddress,
+                        hintMessage: 'Enter Email',
+                        isSensitive: false,
+                        currentTextCallback: (value) {
+                          email = value;
+                        },
+                      ),
                     ),
                   ),
-                ),
-                SizedBox(
-                  height: 5.0,
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: InputTextField(
-                      hintMessage: 'Enter Password',
-                      isSensitive: true,
-                      currentTextCallback: (value) {
-                        password = value;
-                      },
+                  SizedBox(
+                    height: 5.0,
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: InputTextField(
+                        hintMessage: 'Enter Password',
+                        isSensitive: true,
+                        currentTextCallback: (value) {
+                          password = value;
+                        },
+                      ),
                     ),
                   ),
-                ),
-                SizedBox(
-                  height: 10.0,
-                ),
-                ActionButton(
-                  buttonText: 'Login',
-                  buttonActionCallback: () async {
-                    try {
-                      var currentUserCredentials =
-                          await _auth.signInWithEmailAndPassword(
-                              email: email, password: password);
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  ActionButton(
+                    buttonText: 'Login',
+                    buttonActionCallback: () async {
+                      try {
+                        var currentUserCredentials =
+                            await _auth.signInWithEmailAndPassword(
+                                email: email, password: password);
 
-                      Navigator.pushNamed(context, HomeScreen.id);
-                    } catch (e) {
-                      print('error message is : $e');
-                    }
-                  },
-                ),
-              ]),
+                        userDataObject.updateFullProfile();
+                        //replaces all default 'loading...' values with actual values for current user
+                        if (!mounted)
+                          return; //When a BuildContext is used from a StatefulWidget, the mounted property must be checked after an asynchronous gap.
+
+                        Navigator.pushReplacementNamed(context, HomeScreen.id);
+                      } catch (e) {
+                        print('error message is : $e');
+                      }
+                    },
+                  ),
+                ]),
+          ),
         ),
       ),
     );
