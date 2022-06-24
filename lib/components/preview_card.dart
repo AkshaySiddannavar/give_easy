5e3013109e749ddda7fd965b429a409a9703f7c0
Akshay Siddannavar <72383774+AkshaySiddannavar@users.noreply.github.com>
2022-06-24 14:13:10 +0530

    add title to PreviewCard
    Because:
    - It will help users in differentiating preview cards
    - It will help users get an idea about what donation is the preview card about
    
    Added title to PreviewCard.

--- lib/components/preview_card.dart
+++ lib/components/preview_card.dart
@@ -2,52 +2,56 @@ import 'package:flutter/material.dart';
 import 'package:give_easy/constants.dart';
 import 'package:give_easy/screens/all_screens.dart';
 
-//added navigation to specificDonationScreen on tap with GestureDetector
-//how to pass values to specificDonationScreen ? I am using screen id to navigate to it
-
 class PreviewCard extends StatelessWidget {
   final Image previewImage; //image shown in PreviewCard
+  final String title; //title from specific-request-data
 
-  const PreviewCard({
-    Key? key,
-    this.previewImage = kDefaultPreviewImage,
-  }) : super(key: key);
+  const PreviewCard(
+      {Key? key,
+      this.previewImage = kDefaultPreviewImage,
+      this.title = 'Donation For Some Cause'})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      //outer container
-      height: 125.0,
-      width: 125.0,
-      //height and width such that at least 3 of PreviewCards should be visible by default
-      //change it later in case required
+    return GestureDetector(
+      onTap: () {
+        Navigator.pushNamed(context, SpecificDonationScreen.id);
+      },
+      child: Column(
+        children: [
+          Container(
+            height: 125.0,
+            width: 125.0,
+            padding: EdgeInsets.all(10.0),
+            decoration: BoxDecoration(
+                color:
+                    Colors.tealAccent, //to identify extent of outer container
+                borderRadius: BorderRadius.all(Radius.elliptical(20.0, 20.0))),
+            child: Container(
+              //inner container
+              //replace it with padding later on if required
 
-      padding: EdgeInsets.all(10.0),
-      child: GestureDetector(
-        onTap: () {
-          Navigator.pushNamed(context, SpecificDonationScreen.id);
-        },
-        child: Container(
-          //inner container
-          //replace it with padding later on if required
+              decoration: BoxDecoration(
+                  color: Colors.redAccent, //to see extent of container
 
-          decoration: BoxDecoration(
-              color: Colors.redAccent, //to see extent of container
+                  borderRadius: BorderRadius.all(Radius.circular(20.0))),
 
-              borderRadius: BorderRadius.all(Radius.circular(20.0))),
+              child: Flex(
+                  direction: Axis
+                      .horizontal, //change direction in case required later on
 
-          child: Flex(
-              direction:
-                  Axis.horizontal, //change direction in case required later on
-
-              children: [Flexible(child: previewImage)]
-              //We use Flex > Flexible so that we can change size of image from its original size
-              ),
-        ),
+                  children: [Flexible(child: previewImage)]
+                  //We use Flex > Flexible so that we can change size of image from its original size
+                  ),
+            ),
+          ),
+          Text(
+            title.length > 20 ? '${title.substring(0, 19)}...' : title,
+            textAlign: TextAlign.center,
+          )
+        ],
       ),
-      decoration: BoxDecoration(
-          color: Colors.tealAccent, //to identify extent of outer container
-          borderRadius: BorderRadius.all(Radius.elliptical(20.0, 20.0))),
     );
   }
 }
