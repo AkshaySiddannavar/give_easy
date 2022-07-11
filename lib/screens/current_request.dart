338bff1e720b1ac007110fcad2759ea806d3f003
Akshay Siddannavar <72383774+AkshaySiddannavar@users.noreply.github.com>
2022-07-11 19:20:02 +0530

    feature display current requests made by the user
    Because:
    - We want to show a ListView for all the active(currently accepting funds) request made by current user
    - We want to display the name, goalAmount & collectedAmount of each active request
    
    Implemented the feature of displaying basic information about active requests made by the user.

--- lib/screens/current_request.dart
+++ lib/screens/current_request.dart
@@ -1,11 +1,69 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_styled_toast/flutter_styled_toast.dart';
+import 'package:give_easy/components/action_button.dart';
+import 'package:give_easy/request_data_API/request_data_firestore_api.dart';
 
-class CurrentRequestScreen extends StatelessWidget {
+class CurrentRequestScreen extends StatefulWidget {
   static const String id = 'current_request_screen';
   const CurrentRequestScreen({Key? key}) : super(key: key);
 
   @override
+  State<CurrentRequestScreen> createState() => _CurrentRequestScreenState();
+}
+
+class _CurrentRequestScreenState extends State<CurrentRequestScreen> {
+  late Widget listViewOfActiveRequests = Container();
+
+  static String userID = FirebaseAuth.instance.currentUser!.uid;
+
+  Future<Widget> renderActiveRequests() async {
+    List listOfInactiveRequests = await RequestDataAPI.getActiveRequests(
+        userID); //list of 'inactive' documents
+
+    String requestName, goalAmount, collectedAmount;
+    Map requestData;
+    listViewOfActiveRequests = ListView.builder(
+      itemBuilder: ((context, index) {
+        requestData = listOfInactiveRequests[index].data();
+        requestName = requestData["title"];
+        goalAmount = requestData["goalAmount"].toString();
+        collectedAmount = (requestData["collectedAmount"] == -1)
+            ? '0'
+            : requestData["collectedAmount"].toString();
+
+        return ListTile(
+          leading: Text('Name:\n$requestName'),
+          title: Text('Goal Amount:\n$goalAmount'),
+          trailing: Text('Collected Amount:\n$collectedAmount'),
+        );
+      }),
+      itemCount: listOfInactiveRequests.length,
+    );
+
+    return listViewOfActiveRequests;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(title: Text('Current Requests')),
+      body: FutureBuilder(
+          future: renderActiveRequests(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              return Center(child: Text('error'));
+            } else {
+              return listViewOfActiveRequests;
+            }
+          }),
+    );
   }
 }
