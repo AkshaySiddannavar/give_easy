d99f6e8a52e4c8b87987465d1bf84b412094e73d
Akshay Siddannavar <72383774+AkshaySiddannavar@users.noreply.github.com>
2022-07-31 14:05:51 +0530

    make SpecificDonationScreen stateful, fetch data about request from database and use FutureBuilder for rendering data on screen
    Because:
    - We want to load data of request when a user comes to SpecificDonationScreen
    
    Made SpecificDonationScreen stateful, fetching data about request from database and used FutureBuilder for rendering data on screen. Removed donation description from parameters since we are now getting it from database directly in setDonationData function.

--- lib/screens/specific_donation.dart
+++ lib/screens/specific_donation.dart
@@ -1,20 +1,70 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:give_easy/components/action_button.dart';
 import 'package:give_easy/components/title_tile.dart';
 import 'package:give_easy/constants.dart';
 import 'package:give_easy/screens/payment_screen.dart';
+//Gets title from PreviewCard
+//Then fetches data from firestore and renders this screen
+//Title should be unique, one to one title relation with a request (implement this constraint later on)
 
-class SpecificDonationScreen extends StatelessWidget {
+class SpecificDonationScreen extends StatefulWidget {
   static const id = "specific_donation_screen";
+
   final String donationTitle;
-  final String donationDescription;
   final Image donationBackgroundImage;
-  const SpecificDonationScreen(
+
+  SpecificDonationScreen(
       {Key? key,
-      this.donationTitle = 'no title given',
-      this.donationDescription = 'no description given',
+      this.donationTitle = 'NO DATA',
       this.donationBackgroundImage = kDefaultPreviewImage})
       : super(key: key);
+
+  @override
+  State<SpecificDonationScreen> createState() => _SpecificDonationScreenState();
+}
+
+class _SpecificDonationScreenState extends State<SpecificDonationScreen> {
+  late var donationData;
+  static final _firestore = FirebaseFirestore.instance;
+  //later
+
+  Future setDonationData(String title) async {
+    //Sets the value of donationData variable
+
+    print('donation data ran: \ntitle: $title');
+
+    QuerySnapshot<Map<String, dynamic>>? donationDataQueryResult;
+
+    try {
+      donationDataQueryResult = await _firestore
+          .collection("specific-request-data")
+          .where("title", isEqualTo: title)
+          .get()
+          .then((value) {
+        print('value assigned');
+        return value;
+      });
+      //Executing query for collecting data for request having title same as given in paramters
+
+    } catch (e) {
+      print('Erroer roeoroeor \n$e\n*******************');
+    }
+
+    try {
+      donationData = donationDataQueryResult!.docs.first;
+    } catch (e) {
+      print(
+          'error in donation data docs initialzaiont\n*********$e**************');
+
+      donationData = kDefaultRequestData;
+      print(donationData);
+    }
+
+    print('ran wothout problems');
+
+    return donationData;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,48 +73,83 @@ class SpecificDonationScreen extends StatelessWidget {
       floatingActionButton: ActionButton(
         buttonText: 'Fund',
         buttonActionCallback: () {
-          Navigator.pushNamed(context, PaymentScreen.id);
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => PaymentScreen(
+                        organizationName: donationData["organizationName"],
+                        donationDescriptionShort: (donationData["description"]
+                                    .toString()
+                                    .length >
+                                40)
+                            ? "${donationData["description"].toString().substring(0, 40)}..."
+                            : donationData["description"],
+                      )));
         },
         verticalPadding: 0.0,
         horizontalPadding: 0.0,
         buttonColor: Colors.greenAccent,
       ),
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        //   color: Colors.amber,
-        child: Flex(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          mainAxisSize: MainAxisSize.max,
-          direction: Axis.vertical,
-          children: [
-            Expanded(
-              flex: 1,
-              child: TitleTile(
-                title: donationTitle,
-              ),
-            ),
-            Flexible(
-              fit: FlexFit.tight,
-              flex: 3,
-              child: ListView(
+      body: FutureBuilder(
+          future: setDonationData(widget.donationTitle),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            }
+            if (snapshot.hasError) {
+              return Center(
+                  child: Column(
                 children: [
-                  Text(
-                    donationDescription,
-                  ),
                   SizedBox(
-                    height: 10.0,
+                    height: 40.0,
                   ),
                   Text(
-                    'Description ends\nnow it is your choice to support or not \n \n \n \n',
-                    textAlign: TextAlign.center,
-                  )
+                      "Stack Trace ${snapshot.stackTrace.toString().split('')}"),
+                  Text("Error ${snapshot.error.toString()}"),
                 ],
-              ),
-            ),
-          ],
-        ),
-      ),
+              ));
+            } else if (snapshot.hasData) {
+              return Container(
+                width: double.infinity,
+                height: double.infinity,
+                child: Flex(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  mainAxisSize: MainAxisSize.max,
+                  direction: Axis.vertical,
+                  children: [
+                    Expanded(
+                      flex: 1,
+                      child: TitleTile(
+                        title: donationData["title"],
+                      ),
+                    ),
+                    Flexible(
+                      fit: FlexFit.tight,
+                      flex: 3,
+                      child: ListView(
+                        children: [
+                          Text(
+                            donationData["description"],
+                          ), //description ends
+                          SizedBox(
+                            height: 10.0,
+                          ),
+                          Text(
+                            'Description ends\nnow it is your choice to support or not \n \n \n \n',
+                            textAlign: TextAlign.center,
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            } else {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          }),
     );
   }
 }
