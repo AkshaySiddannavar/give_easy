8474b4e699052d89fb73640cc2403b7002a42f92
Akshay Siddannavar <72383774+AkshaySiddannavar@users.noreply.github.com>
2022-08-01 15:34:38 +0530

    initialise firebase functions
    Because:
    - We wanted to use cloud functions to respond to events in cloud firestore
    
    Initialised firebase functions. Updated gitignore to ignore all files and folders managed by Node.
    
    Firebase Project : give-easy
    Language for Cloud Function : typescript
    Dependencies for Cloud Function: managed by Node

---
+++ functions/src/index.ts
@@ -0,0 +1,12 @@
+import * as functions from "firebase-functions";
+
+// Entry point for defining all cloud functions
+
+
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
+
+export const helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
