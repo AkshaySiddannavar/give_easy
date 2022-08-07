84ca9848700c1e87ff48076f1f420c58afc24509
Akshay Siddannavar <72383774+AkshaySiddannavar@users.noreply.github.com>
2022-08-07 15:41:43 +0530

    add getDonationsByUID()
    Because:
    - We want to fetch documents for all donations made by a user
    - We want this data in your-gives sections of app
    
    Added getDonationsByUID().

--- lib/donation_data_API/donation_data_api.dart
+++ lib/donation_data_API/donation_data_api.dart
@@ -1,5 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+// This will act as an API for working with user-donation-data collection in firestore
+
 class DonationDataAPI {
   //add for now
 
@@ -10,5 +12,17 @@ class DonationDataAPI {
     _firestore
         .collection('/user-donation-data')
         .add({"title": donationTitle, "amount": donationAmount, "UID": UID});
+  }
+
+  static Future<List> getDonationsByUID(String UID) async {
+    var queryByUID = await _firestore
+        .collection('/user-donation-data')
+        .where("UID", isEqualTo: UID)
+        .get()
+        .then((value) => value, onError: (error) => print('Error is $error'));
+
+    List donationsByUID = queryByUID.docs;
+
+    return donationsByUID;
   }
 }
