7c9902dc5d053aade72e6af57e0c1fc19a493b3e
Akshay Siddannavar <72383774+AkshaySiddannavar@users.noreply.github.com>
2022-08-08 18:27:15 +0530

    add navigation on tap, make cards thinner, accept title from paramters
    Because:
    - We want to navigate to respective InformationScreen when tapped on InformationCard
    - We want two cards only for now and both should be visible in home screen totally
    
    Added navigation on tap, make cards thinner, accept title from paramters.

--- lib/components/information_card.dart
+++ lib/components/information_card.dart
@@ -1,46 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:give_easy/screens/information_screen.dart';
 
 class InformationCard extends StatelessWidget {
-  final String cardInformation;
-  const InformationCard(
-      {Key? key, this.cardInformation = 'No Information To Display'})
-      : super(key: key);
+  final String displayTextTitle;
+
+  const InformationCard({
+    Key? key,
+    this.displayTextTitle = 'NO TITLE',
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      //outer container
-      height: 250.0,
-      width: 250.0,
-      //height and width such that at least 1 InformationCard should be visible and 2nd partially visible
-      //change it later in case required
-
-      padding: EdgeInsets.all(8.0),
+    return GestureDetector(
+      onTap: () =>
+          Navigator.push(context, MaterialPageRoute(builder: (context) {
+        print(displayTextTitle);
+        return InformationScreen(title: displayTextTitle);
+      })),
       child: Container(
-        //inner container
-        //replace it with padding later on if required
+        //outer container
+        height: 250.0,
+        width: 150.0,
+        //height and width such that at least 1 InformationCard should be visible and 2nd partially visible
+        //change it later in case required
+
+        padding: EdgeInsets.all(8.0),
+        child: Container(
+          //inner container
+          //replace it with padding later on if required
+
+          decoration: BoxDecoration(
+              color: Colors.pinkAccent, //to see extent of container
+
+              borderRadius: BorderRadius.all(Radius.circular(20.0))),
+
+          child: Flex(
+              direction:
+                  Axis.horizontal, //change direction in case required later on
+
+              children: [
+                Flexible(
+                  child: Text(displayTextTitle),
+                ),
+              ]
+              //We use Flex > Flexible so that we can change size of Text from its original size
+              ),
+        ),
 
         decoration: BoxDecoration(
-            color: Colors.pinkAccent, //to see extent of container
-
-            borderRadius: BorderRadius.all(Radius.circular(20.0))),
-
-        child: Flex(
-            direction:
-                Axis.horizontal, //change direction in case required later on
-
-            children: [
-              Flexible(
-                child: Text(cardInformation),
-              ),
-            ]
-            //We use Flex > Flexible so that we can change size of Text from its original size
-            ),
+            color: Colors.lightBlue, //to identify extent of outer container
+            borderRadius: BorderRadius.all(Radius.elliptical(20.0, 20.0))),
       ),
-
-      decoration: BoxDecoration(
-          color: Colors.lightBlue, //to identify extent of outer container
-          borderRadius: BorderRadius.all(Radius.elliptical(20.0, 20.0))),
     );
   }
 }
