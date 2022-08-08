e4e6e9df1e0c7df4fa28dd6ad042ccb24ad505fb
Akshay Siddannavar <72383774+AkshaySiddannavar@users.noreply.github.com>
2022-08-08 20:01:33 +0530

    add alignment, padding and capitalisation
    Because:
    - Better UI/UX
    
    Added alignment, padding and capitalisation.

--- lib/screens/information_screen.dart
+++ lib/screens/information_screen.dart
@@ -33,11 +33,23 @@ class _InformationScreenState extends State<InformationScreen> {
               return Column(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  Text(
-                    widget.title,
-                    style: TextStyle(fontSize: 40),
+                  Padding(
+                    padding: EdgeInsets.only(left: 20),
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        '${widget.title.substring(0, 1).toUpperCase() + widget.title.substring(1)}',
+                        style: TextStyle(fontSize: 40),
+                      ),
+                    ),
                   ),
-                  Text(description),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10),
+                    child: Text(
+                      description,
+                      style: TextStyle(fontSize: 30),
+                    ),
+                  ),
                 ],
               );
             } else if (snapshot.connectionState == ConnectionState.waiting) {
