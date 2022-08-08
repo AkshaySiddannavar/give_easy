a7a9f59edd6fff0a6a7ffdd9a78027bb188429a0
Akshay Siddannavar <72383774+AkshaySiddannavar@users.noreply.github.com>
2022-08-08 18:24:06 +0530

    add functionality to InformationScreen
    Because:
    - We want to display title and its details from firestore when asked for
    
    Added functionality to InformationScreen. It takes String title and displays it along with description.

--- lib/screens/information_screen.dart
+++ lib/screens/information_screen.dart
@@ -1,1 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:give_easy/information_data_API/information_data_api.dart';
 
+class InformationScreen extends StatefulWidget {
+  final String title;
+
+  const InformationScreen({
+    Key? key,
+    this.title = 'NO TITLE',
+  }) : super(key: key);
+
+  @override
+  State<InformationScreen> createState() => _InformationScreenState();
+}
+
+class _InformationScreenState extends State<InformationScreen> {
+  String description = 'NO DESCR';
+
+  Future setDescription(String title) async {
+    description = await InformationDataAPI.getInformation(title);
+
+    return description;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: FutureBuilder(
+          future: setDescription(widget.title),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Column(
+                children: [
+                  Text("Data"),
+                  Text(description),
+                ],
+              );
+            } else if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              return Text("Error is ${snapshot.error.toString()}");
+            } else {
+              return Text('none');
+            }
+          },
+        ));
+  }
+}
