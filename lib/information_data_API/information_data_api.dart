0570599a1235539fd149d61e2177fe9bf07eb877
Akshay Siddannavar <72383774+AkshaySiddannavar@users.noreply.github.com>
2022-08-08 17:50:35 +0530

    add getInformation()
    Because:
    - We want to fetch information for given title from firestore
    
    Added getInformation() which accepts a String title and returns its description.

--- lib/information_data_API/information_data_api.dart
+++ lib/information_data_API/information_data_api.dart
@@ -1,1 +1,19 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 
+class InformationDataAPI {
+  static final _firestore = FirebaseFirestore.instance;
+
+  static Future<String> getInformation(String title) async {
+    QuerySnapshot<Map<String, dynamic>> informationQuerySnapshot =
+        await _firestore
+            .collection('app-specific-terminology')
+            .where("title", isEqualTo: title)
+            .get()
+            .then((value) => value, onError: (error) => print(error));
+
+    var informationDocument = informationQuerySnapshot.docs.first;
+    Map informationContent = informationDocument.data();
+
+    return informationContent["description"];
+  }
+}
